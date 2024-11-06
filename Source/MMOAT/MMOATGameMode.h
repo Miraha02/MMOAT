584ef5fd06265350e3ee1753e509e31601ee222e
Hana <hanadelcourt@gmail.com>
2024-11-06 10:41:57 +0100

    Project init

---
+++ Source/MMOAT/MMOATGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MMOATGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AMMOATGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AMMOATGameMode();
+};
+
+
+
