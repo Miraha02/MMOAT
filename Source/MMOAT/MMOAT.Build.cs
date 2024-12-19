de25fb1bdb6cf6fdf48335e6b0702d7884aa27ab
Hana <hanadelcourt@gmail.com>
2024-12-19 22:06:29 +0100

    Fix all migrations pb

--- Source/MMOAT/MMOAT.Build.cs
+++ Source/MMOAT/MMOAT.Build.cs
@@ -1,13 +1,41 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 using UnrealBuildTool;
+using System.IO;
 
 public class MMOAT : ModuleRules
 {
-	public MMOAT(ReadOnlyTargetRules Target) : base(Target)
-	{
-		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+    public MMOAT(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "GameplayAbilities", "GameplayTasks", "GameplayTags" });
-	}
+        // Ajouter les chemins d'inclusion publics et privés
+        PublicIncludePaths.AddRange(
+            new string[] {
+                Path.Combine(ModuleDirectory, "Public")
+            }
+        );
+
+        PrivateIncludePaths.AddRange(
+            new string[] {
+                Path.Combine(ModuleDirectory, "Private")
+            }
+        );
+
+        // Modules requis pour ce module
+        PublicDependencyModuleNames.AddRange(new string[] 
+        { 
+            "Core", 
+            "CoreUObject", 
+            "Engine", 
+            "InputCore", 
+            "EnhancedInput", 
+            "GameplayAbilities", 
+            "GameplayTasks", 
+            "GameplayTags" 
+        });
+
+        // Modules privés (si nécessaire)
+        PrivateDependencyModuleNames.AddRange(new string[] { });
+    }
 }
