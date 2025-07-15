eee3201cee1f5ba84e36a3206f35ac9d0e9bf209
Hana <hanadelcourt@gmail.com>
2025-07-15 16:54:44 +0200

    [Error-Check] - IMC + IA in Controller

--- Source/MMOAT/Character/Controller/MMOPlayerController.cpp
+++ Source/MMOAT/Character/Controller/MMOPlayerController.cpp
@@ -3,3 +3,21 @@
 
 #include "MMOPlayerController.h"
 
+#include "MMOAT/Err.h"
+
+AMMOPlayerController::AMMOPlayerController()
+{
+	
+}
+
+void AMMOPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// Check que l'IMC et les IA ont été selectionnés
+	IS_NOT_NULL(DefaultMappingContext,"Mapping Context is Null");
+	IS_NOT_NULL(MoveAction,"Move Action is Null");
+	IS_NOT_NULL(LookAction,"Look Action is Null");
+
+	
+}
