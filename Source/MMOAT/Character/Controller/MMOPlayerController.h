0e0c3bb3916cc2c65d2d790e3042736b2254969e
Hana <hanadelcourt@gmail.com>
2025-07-15 16:40:40 +0200

    [Ticket-XnDhGyrh] - [Ticket-gtc4ngEU] - Add Look and Move Action

--- Source/MMOAT/Character/Controller/MMOPlayerController.h
+++ Source/MMOAT/Character/Controller/MMOPlayerController.h
@@ -3,6 +3,8 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "InputAction.h"
+#include "InputMappingContext.h"
 #include "GameFramework/PlayerController.h"
 #include "MMOPlayerController.generated.h"
 
@@ -13,5 +15,36 @@ UCLASS()
 class MMOAT_API AMMOPlayerController : public APlayerController
 {
 	GENERATED_BODY()
+public:
+	AMMOPlayerController();
+
+private:
+	
+protected:
+
+	// Mapping Context & Input Actions
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	UInputMappingContext* DefaultMappingContext;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	UInputAction* MoveAction;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	UInputAction* LookAction;
+
+public:
+
+private:
+	
+protected:
+
+	virtual void BeginPlay() override;
+	virtual void SetupInputComponent() override;
+
+	/** Input callbacks */
+	void Move(const FInputActionValue& Value);
+	void Look(const FInputActionValue& Value);
+
+public:
 	
 };
