b7b197be1da3fbc2af03dba05acb896be72bfac8
Hana <hanadelcourt@gmail.com>
2025-07-17 16:41:38 +0200

    [Ticket-rL8FsEZZ] - Create UdataTable for MMOAT Character Stats

---
+++ Source/MMOAT/Character/Data/MMOATCharacterDataTable.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataTable.h"
+#include "MMOATCharacterDataTable.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MMOAT_API UMMOATCharacterDataTable : public UDataTable
+{
+	GENERATED_BODY()
+	
+protected:
+	
+};
