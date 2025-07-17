7c2b0eb61e70b424deb13d7a3c7981e275942dc9
Hana <hanadelcourt@gmail.com>
2025-07-17 16:58:40 +0200

    [Ticket-rL8FsEZZ] - Set Data in Data Table

--- Source/MMOAT/Character/Data/MMOATCharacterDataTable.h
+++ Source/MMOAT/Character/Data/MMOATCharacterDataTable.h
@@ -9,11 +9,48 @@
 /**
  * 
  */
-UCLASS()
+UCLASS(Blueprintable)
 class MMOAT_API UMMOATCharacterDataTable : public UDataTable
 {
 	GENERATED_BODY()
 	
 protected:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Data", meta=(AllowPrivateAccess=true))
+	float MaxHealth = 100.0f;
 	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Data", meta=(AllowPrivateAccess=true))
+	float MaxMana = 100.0f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Data", meta=(AllowPrivateAccess=true))
+	float HealthRegen = 1.0f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Data", meta=(AllowPrivateAccess=true))
+	float ManaRegen = 1.0f;
+
+public:
+	float GetMaxHealth() const;
+	float GetMaxMana() const;
+	float GetHealthRegen() const;
+	float GetManaRegen() const;
+
+inline float UMMOATCharacterDataTable::GetMaxHealth() const
+{
+	return MaxHealth;
+}
+
+inline float UMMOATCharacterDataTable::GetMaxMana() const
+{
+	return MaxMana;
+}
+
+inline float UMMOATCharacterDataTable::GetHealthRegen() const
+{
+	return HealthRegen;
+}
+
+inline float UMMOATCharacterDataTable::GetManaRegen() const
+{
+	return ManaRegen;
+}
+
 };
