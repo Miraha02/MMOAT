333466ef95122ecc03ac0d9561b31f5bc4cc004b
Hana <hanadelcourt@gmail.com>
2025-07-21 15:32:12 +0200

    [TEST] - Create GE_TakeDamage For Test Purposes Onyl

---
+++ Source/MMOAT/GAS/GE/GE_Test_TakeDamage.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayEffect.h"
+#include "GE_Test_TakeDamage.generated.h"
+
+/**
+ * @warning THIS CLASS SHOULD NEVER BE USED FOR DEVELOPMENT. IT IS FOR TESTING PURPOSE ONLY !
+ */
+UCLASS()
+class MMOAT_API UGE_Test_TakeDamage : public UGameplayEffect
+{
+	GENERATED_BODY()
+
+public:
+
+	float Damage = 5.0f;
+
+	UGE_Test_TakeDamage();
+};
