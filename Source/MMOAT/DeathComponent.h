7d3426b00dd9ccbb717445830232e7e74e2246ef
Hana <hanadelcourt@gmail.com>
2025-07-23 16:12:08 +0200

    [Ticket-CUmq9xbi] - Define all function property logics for DeathComponent

--- Source/MMOAT/DeathComponent.h
+++ Source/MMOAT/DeathComponent.h
@@ -7,14 +7,50 @@
 #include "DeathComponent.generated.h"
 
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnDeathSignature);
+
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class MMOAT_API UDeathComponent : public UActorComponent
 {
 	GENERATED_BODY()
 
+private:
+
+protected:
+	/**
+	 * Indicate if the component Should Destroy The Actor On Death
+	 */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Destroy", meta = (AllowPrivateAccess = "true"))
+	bool bDestroyOnDeath = true;
+
+	/**
+	 * Delay Before Actor Destruction if bDestroyOnDeath = true
+	 */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Destroy", meta = (AllowPrivateAccess = "true"))
+	float DelayBeforeDestroy = 3.0f;
+
+	
+	
+
+public:
+
 public:	
 	// Sets default values for this component's properties
 	UDeathComponent();
+
+	/** Call Character's death */
+	UFUNCTION(BlueprintCallable, Category = "Death")
+	void Die();
+	
+	/** Used to specify actor's death */
+	UFUNCTION(BlueprintNativeEvent, Category = "Death")
+	void HandleDeath();
+	
+	virtual void HandleDeath_Implementation();
+	
+	/** Used to trigger all bound functions */
+	UPROPERTY(BlueprintAssignable, Category = "Death")
+	FOnDeathSignature OnDeath;
 
 protected:
 	// Called when the game starts
