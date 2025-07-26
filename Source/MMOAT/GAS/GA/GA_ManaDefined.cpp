6b58ca55da3982fb2a4ebdd734c3f96814cb3ece
Hana <hanadelcourt@gmail.com>
2025-07-26 17:13:47 +0200

    [Ticket-d07p7esi] - Defind GA_ManaDefined To create GA That Cost Mana By Default

--- Source/MMOAT/GAS/GA/GA_ManaDefined.cpp
+++ Source/MMOAT/GAS/GA/GA_ManaDefined.cpp
@@ -2,3 +2,30 @@
 
 
 #include "GA_ManaDefined.h"
+
+#include "AbilitySystemComponent.h"
+#include "MMOAT/GAS/GE/GE_ManaCost.h"
+
+UGA_ManaDefined::UGA_ManaDefined()
+{
+	CostGameplayEffectClass = UGE_ManaCost::StaticClass();
+}
+
+void UGA_ManaDefined::ApplyCost(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo,
+	const FGameplayAbilityActivationInfo ActivationInfo) const
+{
+	Super::ApplyCost(Handle, ActorInfo, ActivationInfo);
+	
+	if (ManaCost < 0.0f)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Mana Cost can't be Negative !!!"));
+		return;
+	}
+
+	FGameplayEffectSpecHandle CostSpecHandle = MakeOutgoingGameplayEffectSpec(CostGameplayEffectClass, GetAbilityLevel());
+
+	CostSpecHandle.Data->SetSetByCallerMagnitude(FGameplayTag::RequestGameplayTag(FName("Effect.Cost.Mana")), -ManaCost);
+
+	ActorInfo->AbilitySystemComponent->ApplyGameplayEffectSpecToSelf(*CostSpecHandle.Data.Get());
+
+}
--- Source/MMOAT/GAS/GA/GA_ManaDefined.h
+++ Source/MMOAT/GAS/GA/GA_ManaDefined.h
@@ -13,4 +13,28 @@ UCLASS()
 class MMOAT_API UGA_ManaDefined : public UGameplayAbility
 {
 	GENERATED_BODY()
+
+private:
+
+protected:
+	/**
+	 * The Mana Used to cast the spell
+	 */
+	UPROPERTY(EditDefaultsOnly, Category = Mana, meta=(AllowPrivateAccess = "true"))
+	float ManaCost = 50.0f;
+
+public:
+
+private:
+
+protected:
+
+	UGA_ManaDefined();
+	
+	virtual void ApplyCost(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo) const override;
+
+public:
+
+	
+	
 };
