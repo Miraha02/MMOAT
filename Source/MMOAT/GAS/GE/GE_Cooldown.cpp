5ca129ba19b3f8155da9aa76bace96af3a59c847
Hana <hanadelcourt@gmail.com>
2025-07-26 19:03:07 +0200

    [Ticket-d07p7esi] - Init GE_Cooldown

---
+++ Source/MMOAT/GAS/GE/GE_Cooldown.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GE_Cooldown.h"
+
+UGE_Cooldown::UGE_Cooldown()
+{
+}
---
+++ Source/MMOAT/GAS/GE/GE_Cooldown.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayEffect.h"
+#include "GE_Cooldown.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MMOAT_API UGE_Cooldown : public UGameplayEffect
+{
+	GENERATED_BODY()
+
+private:
+
+protected:
+	
+public:
+
+private:
+
+protected:
+
+	UGE_Cooldown();
+	
+public:
+	
+};
