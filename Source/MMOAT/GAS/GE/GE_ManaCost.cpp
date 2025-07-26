3e8158f958c9642ab63fed45a8c0f390f9f49f29
Hana <hanadelcourt@gmail.com>
2025-07-26 16:36:25 +0200

    [Ticket-d07p7esi] - Init Dynamic Mana Cost Gameplay Effect

---
+++ Source/MMOAT/GAS/GE/GE_ManaCost.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GE_ManaCost.h"
+
+UGE_ManaCost::UGE_ManaCost()
+{
+	
+}
---
+++ Source/MMOAT/GAS/GE/GE_ManaCost.h
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayEffect.h"
+#include "GE_ManaCost.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MMOAT_API UGE_ManaCost : public UGameplayEffect
+{
+	GENERATED_BODY()
+
+private:
+
+protected:
+
+public:
+
+private:
+
+protected:
+	
+	UGE_ManaCost();
+
+public:
+};
