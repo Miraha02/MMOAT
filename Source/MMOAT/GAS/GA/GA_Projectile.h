f7063b6b26a31cc840c5b39804ab6b918889e95a
Hana <hanadelcourt@gmail.com>
2025-07-30 16:50:46 +0200

    [Ticket-mYp1lvu1] - Create GA_Projectile

---
+++ Source/MMOAT/GAS/GA/GA_Projectile.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GA_ManaDefined.h"
+#include "GA_Projectile.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MMOAT_API UGA_Projectile : public UGA_ManaDefined
+{
+	GENERATED_BODY()
+	
+};
