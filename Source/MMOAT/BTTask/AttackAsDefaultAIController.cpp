d22e05e7ab2f2f28ec6d7a5cdc135c2a984dda4e
Hana <hanadelcourt@gmail.com>
2025-07-31 21:48:02 +0200

    [Ticket-ff6eDFOw] - Defind Attack BBTask Execution logics

--- Source/MMOAT/BTTask/AttackAsDefaultAIController.cpp
+++ Source/MMOAT/BTTask/AttackAsDefaultAIController.cpp
@@ -3,6 +3,8 @@
 
 #include "AttackAsDefaultAIController.h"
 
+#include "MMOAT/Character/AI/AIController/Default_AIController.h"
+
 UAttackAsDefaultAIController::UAttackAsDefaultAIController()
 {
 	NodeName = "Attack Player";
@@ -11,4 +13,15 @@ UAttackAsDefaultAIController::UAttackAsDefaultAIController()
 EBTNodeResult::Type UAttackAsDefaultAIController::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
 	return Super::ExecuteTask(OwnerComp, NodeMemory);
+
+	AAIController* AIController = OwnerComp.GetAIOwner();
+	ADefault_AIController* Default_AIController = Cast<ADefault_AIController>(AIController->GetPawn());
+
+	if (Default_AIController)
+	{
+		Default_AIController->Default_Attack();
+		return EBTNodeResult::Succeeded;
+	}
+
+	return EBTNodeResult::Failed;
 }
