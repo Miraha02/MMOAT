1928edac37f9c0f0e82e29ae9206ea5a6998b65d
Hana <hanadelcourt@gmail.com>
2025-07-31 15:02:57 +0200

    [Ticket-ff6eDFOw] - Create AICharacter class

---
+++ Source/MMOAT/Character/AI/AICharacter.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "AICharacter.generated.h"
+
+UCLASS()
+class MMOAT_API AAICharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+private:
+
+protected:
+	
+public:
+
+private:
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+
+	// Sets default values for this character's properties
+	AAICharacter();
+	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+};
