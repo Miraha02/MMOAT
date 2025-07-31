91aa7e87e2eab76aaf22f63249ea1cdbad97ae75
Hana <hanadelcourt@gmail.com>
2025-07-31 15:04:46 +0200

    [Ticket-ff6eDFOw] - Create Deafult_AiController Class

---
+++ Source/MMOAT/Character/AI/AIController/Default_AIController.h
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Runtime/AIModule/Classes/AIController.h"
+#include "Default_AIController.generated.h"
+
+UCLASS()
+class MMOAT_API ADefault_AIController : public AAIController
+{
+	GENERATED_BODY()
+
+private:
+
+protected:
+	
+public:
+
+private:
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	
+	// Sets default values for this actor's properties
+	ADefault_AIController();
+	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+};
